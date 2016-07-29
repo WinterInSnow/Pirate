118199e9b53657ef5a2e93b50ff5546836b9ab22
WinterInSnow <Elizabeth@Guanghan-Mac.local>
2016-07-29 13:53:32 -0400

    delete read

--- pirate.py
+++ pirate.py
@@ -1,5 +1,5 @@
 
-with open('treasure.txt') as fh:
-    for line in fh:
-    print line
+with open('map.txt', 'w') as fh:
+  for i in range(10):
+    fh.write("Walk " + str(i) + " paces left.")
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+Just try to run the pirate.py to write output to man.txt and to read from 
+treasure.txt
+
